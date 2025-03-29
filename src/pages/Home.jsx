6793462e340671c8c9cb10d91f71ef5585ee33b9
Andrey9019 <1234andrey5678@gmail.com>
2025-03-29 18:00:16 +0200

    start api servis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import { fetchAllGames } from "../utils/api/all-games";
+
 const Home = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const getGames = async () => {
+      try {
+        const data = await fetchAllGames();
+        setGames(data);
+      } catch (error) {
+        console.error("Error fetching games:", error);
+      }
+    };
+
+    getGames();
+  }, []);
+
   return (
     <section>
       <h1>Welcome to Home Page</h1>
-      <p>This is the home page content.</p>
+
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>
+            <p>{game.title}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
