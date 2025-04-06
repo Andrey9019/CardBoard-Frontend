d507c3523c737027c9817a75a25a9dcc0e681e52
Andrey9019 <1234andrey5678@gmail.com>
2025-04-06 18:41:32 +0300

    update style

--- src/app/components/Main/Hero.tsx
+++ src/app/components/Main/Hero.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Button from "@/components/ui/Button";
 
-
 export default function Hero() {
   return (
-    <section className="bg-background bg-[url('/images/Baner-M.png')] lg:bg-[url('/images/Baner-T.png')] xl:bg-[url('/images/Baner-D.png')] text-secondary mb-12 flex w-full items-center justify-center bg-cover bg-center px-9 py-36 lg:mb-16 lg:px-[154px] lg:py-40 xl:px-[327px] xl:py-[186px]">
+    <section className="bg-background text-secondary mb-12 flex w-full items-center justify-center bg-[url('/images/Baner-M.png')] bg-cover bg-center px-9 py-36 lg:mb-16 lg:bg-[url('/images/Baner-T.png')] lg:px-[154px] lg:py-40 xl:bg-[url('/images/Baner-D.png')] xl:px-[327px] xl:py-[186px]">
       <div className="flex flex-col items-center gap-9">
         <h1 className="text-center text-4xl font-bold lg:text-5xl xl:text-[64px]">
           Насолоджуйтесь найкращими іграми!
