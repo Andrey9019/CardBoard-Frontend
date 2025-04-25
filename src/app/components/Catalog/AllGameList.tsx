13974f86073db6ada7c7f2a5c4edd4d866b0597b
Andrey9019 <1234andrey5678@gmail.com>
2025-04-25 23:11:55 +0300

    add catalog-component in page catalog

--- src/app/components/Catalog/AllGameList.tsx
+++ src/app/components/Catalog/AllGameList.tsx
@@ -10,8 +10,13 @@ import Image from "next/image";
 import Button from "@/components/ui/Button";
 import Game from "@/app/types/interface";
 
-export default function AllGameList() {
+import { FaRegHeart } from "react-icons/fa";
+import SkeletonCard from "../layout/Skeleton";
+
+export default function PopularListGame() {
   const [games, setGames] = useState<Game[]>([]);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getGames = async () => {
@@ -20,26 +25,58 @@ export default function AllGameList() {
         setGames(data);
       } catch (error) {
         console.log("error getGame", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getGames();
   }, []);
   return (
-    <ul className="flex flex-wrap justify-center gap-x-4 gap-y-9">
-      {games.map((game) => (
-        <li
-          key={game.id}
-          className="item-shadow max-w-[196px] rounded-lg bg-white p-4"
-        >
-          <Link href={`/game/${game.id}`}>
-            <button className="items-end">❤️</button>
-            <Image src={noImg} alt="Фото гри" />
-            <p>{game.title}</p>
-            <p>{game.price}</p>
-            <Button type="primary" text="Купити" className="min-w-full" />
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      {isLoading ? (
+        <SkeletonCard />
+      ) : (
+        <ul className="flex flex-wrap justify-center gap-x-4 gap-y-9 sm:justify-between">
+          {games.map((game) => (
+            <li
+              key={game.id}
+              className="item-shadow min-h-[365px] max-w-[196px] rounded-lg bg-white lg:min-h-[429px] lg:max-w-[228px] xl:min-h-[477px] xl:max-w-[270px]"
+            >
+              <Link
+                href={`/game/${game.id}`}
+                className="flex h-full flex-col justify-between p-4"
+              >
+                <div className="flex justify-end">
+                  <button className="max-w-max">
+                    <FaRegHeart className="text-primary h-[18px] w-[18px]" />
+                  </button>
+                </div>
+
+                <Image
+                  className="my-4 flex justify-center"
+                  src={noImg}
+                  alt="Фото гри"
+                />
+
+                <p className="mb-2 line-clamp-2 leading-snug font-bold lg:text-lg">
+                  {game.title}
+                </p>
+                <p className="mb-2 text-lg font-bold lg:text-xl">
+                  {game.price}
+                </p>
+
+                <div className="mt-auto flex flex-col justify-end">
+                  <Button
+                    type="primary"
+                    text="Купити"
+                    className="min-w-full !py-1.5 text-sm uppercase lg:!py-3 lg:text-base"
+                  />
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
