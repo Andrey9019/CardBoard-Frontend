5d684406cdde712c2992997e6e681b6f3f2f0fdb
Andrey9019 <1234andrey5678@gmail.com>
2025-05-10 23:46:11 +0300

    delete tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,44 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        "fade-in-down": {
-          "0%": { opacity: "0", transform: "translateY(-20px)" },
-          "100%": { opacity: "1", transform: "translateY(0)" },
-        },
-        "fade-in-left": {
-          "0%": { opacity: "0", transform: "translateX(-20px)" },
-          "100%": { opacity: "1", transform: "translateX(0)" },
-        },
-      },
-      animation: {
-        "fade-in-down-03": "fade-in-down 0.3s ease-out forwards",
-        "fade-in-down-07": "fade-in-down 0.7s ease-out forwards",
-        "fade-in-down-12": "fade-in-down 1.2s ease-out forwards",
+// const config: Config = {
+//   content: [
+//     "./pages/**/*.{ts,tsx}",
+//     "./components/**/*.{ts,tsx}",
+//     "./app/**/*.{ts,tsx}",
+//     "./src/**/*.{ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         "fade-in-down": {
+//           "0%": { opacity: "0", transform: "translateY(-20px)" },
+//           "100%": { opacity: "1", transform: "translateY(0)" },
+//         },
+//         "fade-in-left": {
+//           "0%": { opacity: "0", transform: "translateX(-20px)" },
+//           "100%": { opacity: "1", transform: "translateX(0)" },
+//         },
+//       },
+//       animation: {
+//         "fade-in-down-03": "fade-in-down 0.3s ease-out forwards",
+//         "fade-in-down-07": "fade-in-down 0.7s ease-out forwards",
+//         "fade-in-down-12": "fade-in-down 1.2s ease-out forwards",
 
-        "fade-in-left-01": "fade-in-left 0.1s ease-out forwards",
-        "fade-in-left-03": "fade-in-left 0.3s ease-out forwards",
-        "fade-in-left-12": "fade-in-left 1.2s ease-out forwards",
-      },
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1440px",
-      "2xl": "1536px",
-    },
-  },
+//         "fade-in-left-01": "fade-in-left 0.1s ease-out forwards",
+//         "fade-in-left-03": "fade-in-left 0.3s ease-out forwards",
+//         "fade-in-left-12": "fade-in-left 1.2s ease-out forwards",
+//       },
+//     },
+//     screens: {
+//       sm: "640px",
+//       md: "768px",
+//       lg: "1024px",
+//       xl: "1440px",
+//       "2xl": "1536px",
+//     },
+//   },
 
-  plugins: [],
-};
+//   plugins: [],
+// };
 
-export default config;
+// export default config;
