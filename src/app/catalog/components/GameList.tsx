335bda6a27980f7b375aa48bd30622049cdb8366
Andrey9019 <1234andrey5678@gmail.com>
2025-05-20 20:24:00 +0300

    restructuring project

--- src/app/catalog/components/GameList.tsx
+++ src/app/catalog/components/GameList.tsx
@@ -1,20 +1,14 @@
 "use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
-import Image from "next/image";
 
 import { getAllGames } from "@/shared/utils";
 import Game from "@/shared/types/game";
 
-import Button from "@/components/ui/Button";
 import SkeletonCard from "../../../components/layout/Skeleton";
-
-import { FaRegHeart } from "react-icons/fa";
-
-import noImg from "../../../../public/images/not-found-page/no-image.png";
-
-import { useRouter } from "next/navigation";
+import Button from "@/components/ui/Button";
+import GameListCard from "./GameListCard";
 
 export default function GameList() {
   const [games, setGames] = useState<Game[]>([]);
@@ -72,42 +66,7 @@ export default function GameList() {
     <>
       <ul className="flex h-max flex-wrap justify-center gap-x-4 gap-y-9 sm:justify-between xl:gap-x-10">
         {games.map((game) => (
-          <li
-            key={game.id}
-            className="item-shadow min-h-[365px] max-w-[196px] rounded-lg bg-white lg:min-h-[429px] lg:max-w-[228px] xl:min-h-[477px] xl:max-w-[270px]"
-          >
-            <Link
-              href={`/game/${game.id}`}
-              className="flex h-full flex-col justify-between p-4"
-            >
-              <div className="flex justify-end">
-                <button className="max-w-max">
-                  <FaRegHeart className="text-primary h-[18px] w-[18px]" />
-                </button>
-              </div>
-
-              <Image
-                className="my-4 flex justify-center"
-                src={noImg}
-                alt="Фото гри"
-              />
-
-              <p className="mb-2 line-clamp-2 leading-snug font-bold lg:text-lg">
-                {game.title}
-              </p>
-              <p className="mb-2 text-lg font-bold lg:text-xl">{game.price}</p>
-
-              <div className="mt-auto flex w-full flex-col justify-end">
-                <Button
-                  as="button"
-                  variant="primary"
-                  text="Купити"
-                  className="min-w-full !py-1.5 text-sm uppercase lg:!py-3 lg:text-base"
-                  onClick={() => {}}
-                />
-              </div>
-            </Link>
-          </li>
+          <GameListCard key={game.id} game={game} />
         ))}
       </ul>
     </>
---
+++ src/app/catalog/components/GameListCard.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import Game from "@/shared/types/game";
+import Button from "@/components/ui/Button";
+import noImg from "../../../../public/images/not-found-page/no-image.png";
+
+import { FaRegHeart } from "react-icons/fa";
+
+interface GameListCard {
+  game: Game;
+}
+
+export default function GameListCard({ game }: GameListCard) {
+  return (
+    <li className="item-shadow min-h-[365px] max-w-[196px] rounded-lg bg-white lg:min-h-[429px] lg:max-w-[228px] xl:min-h-[477px] xl:max-w-[270px]">
+      <Link
+        href={`/game/${game.id}`}
+        className="flex h-full flex-col justify-between p-4"
+      >
+        <div className="flex justify-end">
+          <button className="max-w-max">
+            <FaRegHeart className="text-primary h-[18px] w-[18px]" />
+          </button>
+        </div>
+
+        <Image
+          className="my-4 flex justify-center"
+          src={noImg}
+          alt="Фото гри"
+        />
+
+        <p className="mb-2 line-clamp-2 leading-snug font-bold lg:text-lg">
+          {game.title}
+        </p>
+        <p className="mb-2 text-lg font-bold lg:text-xl">{game.price}</p>
+
+        <div className="mt-auto flex w-full flex-col justify-end">
+          <Button
+            as="button"
+            variant="primary"
+            text="Купити"
+            className="min-w-full !py-1.5 text-sm uppercase lg:!py-3 lg:text-base"
+            onClick={() => {}}
+          />
+        </div>
+      </Link>
+    </li>
+  );
+}
