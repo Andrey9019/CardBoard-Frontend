ff05df28fd2c259e9e8fb20cb493939e9693d78b
Andrey9019 <1234andrey5678@gmail.com>
2025-06-03 18:45:04 +0300

    add loader in cart

---
+++ src/components/ui/index.ts
@@ -0,0 +1,34 @@
+export { Checkbox } from "./ckeckbox";
+export { Skeleton } from "./skeleton";
+export { Badge, badgeVariants } from "./badge";
+export { Button, buttonVariants } from "./ButtonPagination";
+
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+  AccordionTriggerFilter,
+} from "./accordion";
+
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+} from "./breadcrumb";
+
+export {
+  Pagination,
+  PaginationContent,
+  PaginationLink,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
+} from "./pagination";
+
+export {} from "";
