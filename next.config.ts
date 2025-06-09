330b98457e13a7e2e95daa5dc75526e234602e32
Andrey9019 <1234andrey5678@gmail.com>
2025-06-09 21:16:55 +0300

    server request has been made

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      process.env.NEXT_PUBLIC_DB_API_BASE_URL?.replace(/https?:\/\//, "") ||
-        "board-games-back-1-0-0.onrender.com",
-      "localhost",
-    ],
+    domains: ["board-games-back-1-0-0.onrender.com", "localhost"],
   },
 };
 
