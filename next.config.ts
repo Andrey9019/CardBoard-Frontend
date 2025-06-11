d6d04ab2e221ae24a3a1817d1abee6f200d248e3
Andrey9019 <1234andrey5678@gmail.com>
2025-06-11 14:36:55 +0300

    server request for img

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["board-games-back-1-0-0.onrender.com", "localhost"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/media/:path*",
+        destination: "https://board-games-back-1-0-0.onrender.com/media/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/(catalog)/game/[id]/components/GameListImages.tsx
+++ src/app/(catalog)/game/[id]/components/GameListImages.tsx
@@ -4,9 +4,9 @@ import { useImagesByID } from "@/shared/hooks/useImagesById";
 import { useState, useEffect, useCallback } from "react";
 import { EmblaOptionsType } from "embla-carousel";
 import useEmblaCarousel from "embla-carousel-react";
-// import noImg from "../../../../../../public/images/not-found-page/no-image.png";
+import noImg from "../../../../../../public/images/not-found-page/no-image.png";
 
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function GameListImagesProps() {
   const options: EmblaOptionsType = {};
@@ -40,25 +40,22 @@ export default function GameListImagesProps() {
     emblaMainApi.on("select", onSelect).on("reInit", onSelect);
   }, [emblaMainApi, onSelect]);
 
+  console.log("Environment URL:", process.env.NEXT_PUBLIC_DB_API_BASE_URL);
+  console.log("Image path on prod:", images?.[0]?.path);
+
   return (
     <div className="ml:max-w-[580px] mx-auto mb-12 max-w-[768px] [--slide-size:100%] xl:mb-0">
       <div className="overflow-hidden" ref={emblaMainRef}>
         <div className="ml:max-w-[580px] ml-[calc(var(--slide-spacing)_*-1)] flex [touch-action:pan-y_pinch-zoom] gap-4">
           {images?.map((image, index) => (
-            // <Image
-            //   className="ml:max-w-[588px] shrink-0 grow-0 basis-[var(--slide-size)] [transform:translate3d(0,0,0)] rounded-lg border-1 border-black pl-[var(--slide-spacing)]"
-            //   key={index}
-            //   src={image.path || noImg}
-            //   alt={`Game image ${index + 1}`}
-            //   width={300}
-            //   height={300}
-            // />
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              key={index}
-              src={decodeURIComponent(image.path)}
-              alt={`Image ${index}`}
+            <Image
               className="ml:max-w-[588px] shrink-0 grow-0 basis-[var(--slide-size)] [transform:translate3d(0,0,0)] rounded-lg border-1 border-black pl-[var(--slide-spacing)]"
+              key={index}
+              src={image.path || noImg}
+              alt={`Game image ${index + 1}`}
+              width={300}
+              height={300}
+              priority={index === 0}
             />
           ))}
         </div>
@@ -67,26 +64,17 @@ export default function GameListImagesProps() {
       <div className="mt-4 overflow-hidden" ref={emblaThumbsRef}>
         <div className="ml-[calc(var(--thumbs-slide-spacing)_*_ -1)] flex flex-row">
           {images?.map((image, index) => (
-            // <Image
-            //   onClick={() => onThumbClick(index)}
-            //   key={index}
-            //   src={image.path || noImg}
-            //   alt={`Thumbnail image ${index + 1}`}
-            //   width={96}
-            //   height={96}
-            //   className={"mr-3 max-w-24 flex-[0_0_22%] rounded-lg border-1 border-black pl-[var(--thumbs-slide-spacing)]".concat(
-            //     index === selectedIndex ? "text-[var(--text-body)]" : "",
-            //   )}
-            // />
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
+            <Image
+              onClick={() => onThumbClick(index)}
               key={index}
-              src={decodeURIComponent(image.path)}
-              alt={`Image ${index}`}
-              className={"bord/er-1 mr-3 max-w-24 flex-[0_0_22%] rounded-lg border-black pl-[var(--thumbs-slide-spacing)]".concat(
+              src={image.path || noImg}
+              alt={`Thumbnail image ${index + 1}`}
+              width={96}
+              height={96}
+              priority={index === 0}
+              className={"mr-3 max-w-24 flex-[0_0_22%] rounded-lg border-1 border-black pl-[var(--thumbs-slide-spacing)]".concat(
                 index === selectedIndex ? "text-[var(--text-body)]" : "",
               )}
-              onClick={() => onThumbClick(index)}
             />
           ))}
         </div>
