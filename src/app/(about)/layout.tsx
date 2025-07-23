84ea81c10c49cbec57a3ddf051609b864dda6c51
Andrey9019 <1234andrey5678@gmail.com>
2025-07-23 18:17:17 +0300

    minor update

--- src/app/(about)/layout.tsx
+++ src/app/(about)/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header/Header";
 
 export default function InfoLayout({
   children,
