bd97717d7d7780dc49631b68d4bc83134cb8c398
Andrey9019 <1234andrey5678@gmail.com>
2025-10-06 16:53:11 +0300

    add register page/login folder/dashboard folder

---
+++ src/app/(auth)/register/components/RegisterBody.tsx
@@ -0,0 +1,3 @@
+export default function RegisterBody() {
+    return(<p>Register</p>)
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+import RegisterBody from "./components/RegisterBody";
+
+export default function Register() {
+return (
+<RegisterBody/>
+);
+}
